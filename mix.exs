8d08077f56fab08d2bc5ea3564a1ce853e8f2d3f
Sam Corcos <sam@corcos.io>
2016-09-11 15:29:05 -0400

    finish user accounts and signup two

--- mix.exs
+++ mix.exs
@@ -18,8 +18,17 @@ defmodule PhoenixChat.Mixfile do
   # Type `mix help compile.app` for more information.
   def application do
     [mod: {PhoenixChat, []},
-     applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :postgrex]]
+      applications: [
+        :comeonin,
+        :cowboy,
+        :gettext,
+        :logger,
+        :phoenix,
+        :phoenix_ecto,
+        :phoenix_html,
+        :phoenix_pubsub,
+        :postgrex
+      ]]
   end
 
   # Specifies which paths to compile per environment.
@@ -30,14 +39,18 @@ defmodule PhoenixChat.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.2.0"},
-     {:phoenix_pubsub, "~> 1.0"},
-     {:phoenix_ecto, "~> 3.0"},
-     {:postgrex, ">= 0.0.0"},
-     {:phoenix_html, "~> 2.6"},
-     {:phoenix_live_reload, "~> 1.0", only: :dev},
-     {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"}]
+    [
+      {:comeonin, "~> 2.3"},
+      {:corsica, "~> 0.4"},
+      {:cowboy, "~> 1.0"},
+      {:gettext, "~> 0.11"},
+      {:phoenix, "~> 1.2.0"},
+      {:phoenix_pubsub, "~> 1.0"},
+      {:phoenix_ecto, "~> 3.0"},
+      {:phoenix_html, "~> 2.6"},
+      {:phoenix_live_reload, "~> 1.0", only: :dev},
+      {:postgrex, ">= 0.0.0"}
+    ]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
